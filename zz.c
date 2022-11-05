be0c34a9aad27389aaa04cc4ecb52628d4adb316
236375252 <3275774040@qq.com>
2022-11-05 16:39:16 +0800

    c

---
+++ zz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=100, b=200;
+	int *p1,*p2=&b;
+	p1=&a;//&取地址
+	int num;
+	num=*p1;//*取值 与num=a相同
+	printf("%d\n",a);
+	printf("%d\n",*p1);
+	printf("%d\n",num);
+	printf("%d\n",*p2);
+	return 0;
+}
+
+
+
